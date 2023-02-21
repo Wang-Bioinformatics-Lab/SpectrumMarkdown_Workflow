ef8f99c3e14baaa7e50ba363bdad2a386899e7cd
Mingxun Wang <mwang87@gmail.com>
2023-02-20 22:09:15 -0800

    bug fix

--- bin/createusimarkdown.py
+++ bin/createusimarkdown.py
@@ -17,13 +17,6 @@ df = pd.read_csv(args.input_filename, sep="\t")
 df["image_path"] = df["usi"].apply(lambda x: '{}/{}.svg'.format(args.output_images_folder, uuid.uuid4()))
 df["image"] = df["image_path"].apply(lambda x: '![]({})'.format(x))
 
-# drop image_path
-df = df.drop(columns=["image_path"])
-
-open(args.output_markdown, 'w').write(df.to_markdown(index=False))
-open(args.output_html, "w").write(markdown.markdown(df.to_markdown(index=False), extensions=['tables']))
-
-# TODO: Download all the images directly so we don't rely on server
 for results_obj in df.to_dict(orient="records"):
     usi = results_obj["usi"]
     if "usi2" in results_obj:
@@ -44,4 +37,8 @@ for results_obj in df.to_dict(orient="records"):
     except:
         pass
 
+# drop image_path
+df = df.drop(columns=["image_path"])
 
+open(args.output_markdown, 'w').write(df.to_markdown(index=False))
+open(args.output_html, "w").write(markdown.markdown(df.to_markdown(index=False), extensions=['tables']))
