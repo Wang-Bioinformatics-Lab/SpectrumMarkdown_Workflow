1587d2ff75a32f1718c6fa71fe55840db3b2793e
Mingxun Wang <mwang87@gmail.com>
2024-01-18 16:23:02 -0800

    updating

--- bin/createusimarkdown.py
+++ bin/createusimarkdown.py
@@ -5,66 +5,72 @@ import markdown
 import uuid
 import requests
 
-parser = argparse.ArgumentParser(description='Doing some conversions')
-parser.add_argument('input_filename')
-parser.add_argument('output_markdown')
-parser.add_argument('output_html')
-parser.add_argument('output_images_folder')
+def main():
 
-args = parser.parse_args()
+    parser = argparse.ArgumentParser(description='Doing some conversions')
+    parser.add_argument('input_filename')
+    parser.add_argument('output_markdown')
+    parser.add_argument('output_html')
+    parser.add_argument('output_images_folder')
 
-df = pd.read_csv(args.input_filename, sep="\t")
-df["image_path"] = df["usi"].apply(lambda x: '{}/{}.svg'.format(args.output_images_folder, uuid.uuid4()))
-df["image"] = df["image_path"].apply(lambda x: '![]({})'.format(x))
+    args = parser.parse_args()
 
-for results_obj in df.to_dict(orient="records"):
-    usi = results_obj["usi"]
-    if "usi2" in results_obj:
-        usi2 = results_obj["usi2"]
-    else:
-        usi2 = None
-    
-    image_path = results_obj["image_path"]
+    df = pd.read_csv(args.input_filename, sep="\t")
+    df["image_path"] = df["usi"].apply(lambda x: '{}/{}.svg'.format(args.output_images_folder, uuid.uuid4()))
+    df["image"] = df["image_path"].apply(lambda x: '![]({})'.format(x))
 
-    params = {}
+    for results_obj in df.to_dict(orient="records"):
+        usi = results_obj["usi"]
+        if "usi2" in results_obj:
+            usi2 = results_obj["usi2"]
+        else:
+            usi2 = None
+        
+        image_path = results_obj["image_path"]
 
-    if usi2 is not None:
-        url = "https://metabolomics-usi.gnps2.org/svg/mirror/".format(usi, usi2)
-        params["usi1"] = usi
-        params["usi2"] = usi2
-    else:
-        url = "https://metabolomics-usi.gnps2.org/svg/".format(usi)
-        params["usi1"] = usi
+        params = {}
 
-    if "mz_max" in results_obj:
-        params["mz_max"] = results_obj["mz_max"]
+        if usi2 is not None:
+            url = "https://metabolomics-usi.gnps2.org/svg/mirror/".format(usi, usi2)
+            params["usi1"] = usi
+            params["usi2"] = usi2
+        else:
+            url = "https://metabolomics-usi.gnps2.org/svg/".format(usi)
+            params["usi1"] = usi
 
-    if "mz_min" in results_obj:
-        params["mz_min"] = results_obj["mz_min"]
+        if "mz_max" in results_obj:
+            params["mz_max"] = results_obj["mz_max"]
 
-    if "max_intensity" in results_obj:
-        params["max_intensity"] = results_obj["max_intensity"]
+        if "mz_min" in results_obj:
+            params["mz_min"] = results_obj["mz_min"]
 
-    
-    
-    try:
-        r = requests.get(url, params=params)
-        open(image_path, "wb").write(r.content)
-    except:
-        pass
+        if "max_intensity" in results_obj:
+            params["max_intensity"] = results_obj["max_intensity"]
 
-# drop image_path
-df = df.drop(columns=["image_path"])
+        
+        
+        try:
+            r = requests.get(url, params=params)
+            open(image_path, "wb").write(r.content)
+        except:
+            pass
 
-if "mz_max" in df.columns:
-    df = df.drop(columns=["mz_max"])
+    # drop image_path
+    df = df.drop(columns=["image_path"])
 
-if "mz_min" in df.columns:
-    df = df.drop(columns=["mz_min"])
+    if "mz_max" in df.columns:
+        df = df.drop(columns=["mz_max"])
 
-if "max_intensity" in df.columns:
-    df = df.drop(columns=["max_intensity"])
+    if "mz_min" in df.columns:
+        df = df.drop(columns=["mz_min"])
+
+    if "max_intensity" in df.columns:
+        df = df.drop(columns=["max_intensity"])
 
 
-open(args.output_markdown, 'w').write(df.to_markdown(index=False))
-open(args.output_html, "w").write(markdown.markdown(df.to_markdown(index=False), extensions=['tables']))
+    open(args.output_markdown, 'w').write(df.to_markdown(index=False))
+    open(args.output_html, "w").write(markdown.markdown(df.to_markdown(index=False), extensions=['tables']))
+
+
+if __name__ == '__main__':
+    main()
